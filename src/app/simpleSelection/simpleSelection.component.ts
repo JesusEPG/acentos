93a22e3d653d0bcd8269f9e45b3e3e71c6a0d177
Jesús Pernía <jesuse.epg@gmail.com>
2018-04-02 21:53:49 -0400

    Definiendo ingreso de respuestas correctas y respuestas posibles

--- src/app/simpleSelection/simpleSelection.component.ts
+++ src/app/simpleSelection/simpleSelection.component.ts
@@ -9,8 +9,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SimpleSelectionComponent implements OnInit {
 	activityForm: FormGroup;
 	splittedString: any[];
-	correctAnswers: any[];
-	possibleAnswers: any[];
+	correctAnswers: any[]=[];
+	possibleAnswers: any[]=[];
 
 	//constructor(private authService: AuthService){}
 
@@ -18,6 +18,7 @@ export class SimpleSelectionComponent implements OnInit {
 		this.activityForm = new FormGroup({
 			firstName: new FormControl(null, Validators.required),
 			lastName: new FormControl(null, Validators.required),
+			possibleAnswer: new FormControl(null, Validators.required),
 			fullString: new FormControl(null, [
 				Validators.required//,
 				//Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
@@ -42,21 +43,22 @@ export class SimpleSelectionComponent implements OnInit {
 
 		//Validar que si 'token' es un signo de puntuación, se debe colocar 'cliackeable:false'
 		//Y en el cliente solo se muestran los que tengan 'clickeable:true'
-		this.splittedString = tokens.map(function(token) {
+		this.splittedString = tokens.map(function(token, index) {
 
 			//Verificar si es un caracter especial, no es clickeable.
 			if(/^[a-zA-ZáÁéÉíÍóÓúÚ]+$/.test(token)){
 				
-				//Se debe agregar al arreglo de respuestas correctas y de respuestas posibles
-				//Analizar los casos en que se deben ocultar o no estas palabras
-				//O si se les puede hacer click
-				return {				
+				return {
+					//id: window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now(),				
+				   	id: index,
 				   	word: token,
 				   	hidden: false,
 				   	clickeable: true
 				}
 			}
 			return {
+				//id: window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now(),
+				id: index,
 				word: token,
 			   	hidden: false,
 			   	clickeable: false
@@ -67,24 +69,67 @@ export class SimpleSelectionComponent implements OnInit {
 
 	hideAnswer(word){
 
+		//De ser seleccionada una palabra, es decir word.hidden == false
+		//Se hace word.hidden = false y
+		//Se debe agregar al arreglo de respuestas correctas y de respuestas posibles
+		//Analizar los casos en que se deben ocultar o no estas palabras
+		//O si se les puede hacer click
+
+		if(!word.hidden){
+			
+			this.addCorrectAnswer(word);
+			
+		} else {
+			this.deleteCorrectAnswer(word);
+		}
 		word.hidden = !word.hidden;
-
-		/*let newWord = this.splittedString.find( token => token.id === word.id );
-
-		newWord.hidden = !word.hidden;
-
-		var foundIndex = this.splittedString.findIndex(token => token.id == word.id);
-		this.splittedString[foundIndex] = newWord;*/
-
-		console.log(this.splittedString);
+		//De lo contrario se debe buscar el objeto en los arreglos y luego sacarlo
 	}
 
 	log(){
 		console.log(this.splittedString);
+		console.log(this.correctAnswers);
+		console.log(this.possibleAnswers);
 	}
 
-	hasOnlywords(word){
-		return /^[a-zA-Z\-_ ’'‘ÆÐƎƏƐƔĲŊŒẞÞǷȜæðǝəɛɣĳŋœĸſßþƿȝĄƁÇĐƊĘĦĮƘŁØƠŞȘŢȚŦŲƯY̨Ƴąɓçđɗęħįƙłøơşșţțŧųưy̨ƴÁÀÂÄǍĂĀÃÅǺĄÆǼǢƁĆĊĈČÇĎḌĐƊÐÉÈĖÊËĚĔĒĘẸƎƏƐĠĜǦĞĢƔáàâäǎăāãåǻąæǽǣɓćċĉčçďḍđɗðéèėêëěĕēęẹǝəɛġĝǧğģɣĤḤĦIÍÌİÎÏǏĬĪĨĮỊĲĴĶƘĹĻŁĽĿʼNŃN̈ŇÑŅŊÓÒÔÖǑŎŌÕŐỌØǾƠŒĥḥħıíìiîïǐĭīĩįịĳĵķƙĸĺļłľŀŉńn̈ňñņŋóòôöǒŏōõőọøǿơœŔŘŖŚŜŠŞȘṢẞŤŢṬŦÞÚÙÛÜǓŬŪŨŰŮŲỤƯẂẀŴẄǷÝỲŶŸȲỸƳŹŻŽẒŕřŗſśŝšşșṣßťţṭŧþúùûüǔŭūũűůųụưẃẁŵẅƿýỳŷÿȳỹƴźżžẓ]$/.test(word);
+	addCorrectAnswer(word){
+		//word.clickeable = !word.clickeable;
+		this.correctAnswers.push(word);
+		this.possibleAnswers.push(word);
+	}
+
+	deleteCorrectAnswer(word){
+		//word.clickeable = !word.clickeable;
+		//Buscar el objeto y luego eliminarlo
+		this.remove(this.correctAnswers, word);
+		this.remove(this.possibleAnswers, word);
+
+	}
+
+	addPossibleAnswer(){
+		let str = this.activityForm.value.possibleAnswer;
+		const word = {
+				   	id: this.possibleAnswers.length,
+				   	word: str,
+				   	hidden: false,
+				   	clickeable: true
+				};
+		this.possibleAnswers.push(word);
+	}
+
+	deletePossibleAnswer(word){
+		//word.clickeable = !word.clickeable;
+		this.remove(this.possibleAnswers, word);
+	}
+
+	hasOnlywords(word){}
+
+	remove(array, element) {
+	    const index = array.indexOf(element);
+    
+	    if (index !== -1) {
+	        array.splice(index, 1);
+	    }
 	}
 
 	onSubmit(){}
